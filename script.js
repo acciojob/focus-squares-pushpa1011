42d408b59b3aa26526b0366865cd0fc7a47c4788
pushpa1011 <105695742+pushpa1011@users.noreply.github.com>
2026-07-06 16:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+
+    square.addEventListener("mouseenter", () => {
+
+        squares.forEach((box) => {
+
+            if (box !== square) {
+                box.style.backgroundColor = "#6F4E37"; // Coffee
+            } else {
+                box.style.backgroundColor = "#E6E6FA"; // Lavender
+            }
+
+        });
+
+    });
+
+    square.addEventListener("mouseleave", () => {
+
+        squares.forEach((box) => {
+            box.style.backgroundColor = "#E6E6FA";
+        });
+
+    });
+
+});
